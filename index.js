af42685622566fd4f66343b3189a08388f8b45b1
imran <imranaha310@gmail.com>
2022-10-19 10:22:28 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,10 @@
+let btn=document.getElementById('runButton');
+
+btn.addEventListener('click',function(){
+    let codeInput=document.getElementById('input').value;
+    document.getElementById('output').innerHTML=codeInput;
+    
+    
+    
+})
+
